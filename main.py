1d98a08f222c1b8979ef8ff0292efd18e3310058
Brandon Peña <brandon.pena96@gmail.com>
2023-12-18 10:15:42 -0300

    first working version

--- main.py
+++ main.py
@@ -1,26 +1,82 @@
 import utils
 import json
 import pandas as pd
+from datasets import Dataset, DatasetDict
+from sklearn.model_selection import train_test_split
 
-dataset_len = utils.X_train.size
+dataset_len = utils.X.size
 
 i = 0
+step = 3
 dataset_dict = {'text':[], 'label':[] }
 while i < dataset_len:
-    end = i+10
-    if i+10 >= dataset_len:
+    print("índice: ",i, "\n")
+    end = i+step
+    if i+step >= dataset_len:
         end = dataset_len
-    text = "``".join(utils.X_train[i:end]) 
+    text = "```".join(utils.X[i:end])
+    print(text) 
     completion = utils.get_completion(utils.prompt, text)   
     print(completion)
-    json_obj = json.loads(completion)
+    try:
+        json_obj = json.loads(completion)
+    except ValueError as e:
+        print("value Error! gpt got the json wrong")
+        i+=step
+        continue
+        
     #add classes to classes array
+    largo_clases = len(json_obj['classes'])
+    largo_traduciones = len(json_obj['translations'])
+    print("largo de clases ", largo_clases)
+    print("largo de traducciones ", largo_traduciones)
+    if largo_clases != largo_traduciones :
+        i+=step
+        continue
     new_classes = json_obj['classes']
     dataset_dict['label'].extend(new_classes)
     #add translations to translations array
     new_translations = json_obj['translations']
-    dataset_dict['text']
-    i+=10
+    dataset_dict['text'].extend(new_translations)
+    i+=step
     
     
 sexism_dataframe = pd.DataFrame(dataset_dict)
+grooming_data = sexism_dataframe[sexism_dataframe['label']=='GP']
+grooming_data.to_excel("grooming.xlsx")
+grooming_data.to_csv("grooming.csv")
+
+# I have to change the label form 'GP' to 4, since the labels in offendes go from 0 to 3 ..
+
+grooming_data.loc[grooming_data['label']=='GP', 'label'] = 4
+
+train, test = train_test_split(grooming_data, test_size=0.2)
+train, validation = train_test_split(train, test_size= 0.3)
+train_list = [utils.new_offendEs_train, train]
+validation_list = [utils.new_offendEs_validation, validation]
+test_list = [utils.new_offendEs_test, test]
+
+train_dataframe = pd.concat(train_list, ignore_index=True)
+validation_dataframe = pd.concat(validation_list, ignore_index=True)
+test_dataframe = pd.concat(test_list, ignore_index=True)
+
+hugg_train = Dataset.from_pandas(train_dataframe)
+hugg_validation = Dataset.from_pandas(validation_dataframe)
+hugg_test = Dataset.from_pandas(test_dataframe)
+
+
+new_data_dict = DatasetDict(
+    {
+        "train": hugg_train,
+        "validation": hugg_validation,
+        "test": hugg_test,
+    }
+)
+
+new_data_dict.save_to_disk()
+new_data_dict['train'].to_csv("cyberdefender_train.csv")
+new_data_dict['validation'].to_csv("cyberdefender_validation.csv")
+new_data_dict['test'].to_csv("cyberdefender_test.csv")
+new_data_dict['train'].to_csv("cyberdefender_train.json")
+new_data_dict['validation'].to_csv("cyberdefender_validation.json")
+new_data_dict['test'].to_csv("cyberdefender_test.json")
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from huggingface_hub import login
 from datasets import load_dataset
 import pandas as pd
-from sklearn.model_selection import train_test_split
+
 
 load_dotenv()
 
@@ -26,36 +26,121 @@ def get_completion(prompt, text,  model="gpt-3.5-turbo"):
     )
     return response.choices[0].message.content
 
-prompt = """Eres un detector de texto potencialmente peligroso para niños. 
-    Tienes que clasificar los textos entregados por el usuario que estarán separados por doble backticks en 
-    las siguientes categorias:
-    -OFP: Mensajes con contenido ofensivo dirigido directo al receptor.
-    -OFG: Mensajes con contenido ofensivo a un grupo de personas de cualquier índole.
-    -NO: Texto no ofensivo ni peligroso.
-    -NOE: No ofensivo ni peligroso, pero explícito en su contenido.
-    -GP: Posible grooming o acoso sexual hacia el infante.
+prompt = """Eres un sistema de detección de texto diseñado para proteger a los niños de posibles amenazas en línea. Tu tarea es clasificar y traducir al español los textos proporcionados por el usuario, que estarán separados entre sí por triple comillas invertidas (```). El contenido se clasificará en una de las siguientes categorías, según los criterios que se indican a continuación.
 
-    El formato de entrega es json, de la siguiente manera, lo que debes entregar estará entre un signo menor y uno mayor <de esta forma>:
-    
-    {
-        "classes": <las clases separadas por comas y como string>,
-        "translations": <las traducciones al español de cada una de las entradas separadas por doble backtics>
-    }
+- OFP: Mensajes con contenido ofensivo dirigido directamente al receptor, como insultos, amenazas o humillaciones.
+- OFG: Mensajes con contenido ofensivo dirigido a un grupo de personas de cualquier índole, como discriminación o odio.
+- NO: Texto no ofensivo ni peligroso.
+- NOE: No ofensivo ni peligroso, pero explícito en su contenido, como lenguaje sexual o violencia.
+- GP: Posible grooming o acoso sexual hacia el infante.
+
+Es muy importante que tengas en consideración al clasificar que el objetivo de esta clasificación es proteger a niños.
+El formato de entrega debe ser en JSON, encerrado entre un signo menor y mayor <de esta forma> se encuentra la descripción de cada campo. A continuación el formato:
+
+{
+    "count": <número de mensajes separados por triple comilla invertida>,
+    "classes": <las clases como un arreglo de string, donde cada clase es un string por separado>,
+    "translations": <las traducciones al español de las entradas separadas por triple comilla invertida, en un arreglo de strings con cada traducción como un string por separado>
+}
     
     Ejemplo de interacción:
     
-    user: Hi how are you?``Women should stay in the kitchen
+    usuario: Hi how are you?```Women should stay in the kitchen```gtfo fat fuck!
     
     gpt: {
+        "count":3,
         "classes": [
             "NO", 
-            "OFG"
+            "OFG",
+            "OFP"
             ],
         "translations":[
             "Hola ¿Cómo estás?",
-            "Las mujeres deberían permanecer en la cocina"
+            "Las mujeres deberían permanecer en la cocina",
+            "¡Sal de aqui tipejo gordo!"
         ]    
     }
+    
+    Ejemplos incorrectos de interacción:
+    
+    usuario: Hi how are you?```Women "should" stay in the kitchen```gtfo fat fuck!
+    
+    gpt: {
+        "count": 3,
+        "classes": [
+            "NO", 
+            "OFG",
+            "OFP"
+            ],
+        "translations":[
+            "Hola ¿Cómo estás?",
+            "Las mujeres "deberían" permanecer en la cocina",
+            "¡Sal de aqui tipejo gordo!"
+        ]    
+    }
+    
+    Esta respuesta es incorrecta puesto que al usar las mismas comillas (") que delimitan el string, se parte el string en dos. Al traducir no puedes
+    entregar respuestas que contengan comillas (") dentro de las traducciones. JAMÁS debes entregar respuestas como esta.
+    
+    usuario: Hi how are you?```Women should stay in the kitchen```gtfo fat fuck!
+    
+    gpt: {
+        "count": 3,
+        "classes": [
+            "NO", 
+            "OFG",
+            "OFP",
+            "NO"
+            ],
+        "translations":[
+            "Hola ¿Cómo estás?",
+            "Las mujeres deberían permanecer en la cocina",
+            "¡Sal de aqui tipejo gordo!"
+        ]    
+    }
+    
+    El número de traducciones no coincide con el número de clases, ni con el número de oraciones contadas. Es importante
+    que coincidan puesto que estos datos serán usados en un dataset.
+    
+    usuario: Hi how are you?```Women should stay in the kitchen```gtfo fat fuck!
+    
+    gpt: {
+        "count": 5,
+        "classes": [
+            "NO", 
+            "OFG",
+            "OFP"
+            ],
+        "translations":[
+            "Hola ¿Cómo estás?",
+            "Las mujeres deberían permanecer en la cocina",
+            "¡Sal de aqui tipejo gordo!"
+        ]    
+    }
+    El número no coincide con la cantidad real, por lo que esta respuesta es incorrecta.
+    
+    usuario: Hi how are you?```A woman in the kitchen -as she should-```gtfo fat fuck!
+    
+    gpt: {
+        "count": 3,
+        "classes": [
+            "NO", 
+            "OFG",
+            "OFP",
+            "NO"
+            ],
+        "translations":[
+            "Hola ¿Cómo estás?",
+            "Una mujer en la cocina "como debería estar".",
+            "¡Sal de aqui tipejo gordo!"
+        ]    
+    }
+    
+    Esta respuesta es incorrecta dado que reemplaza un caracter para-verbal (en este caso -) por las comillas ("). 
+    
+    Siempre te entregarán tres mensajes o menos, debes asegurarte que el número de clases entregado en el JSON coincida con el número de traducciones. 
+    Se especialmente cuidadoso al momento de generar los strings de las traducciones, cambia las comillas (") dentro del string de las traducciones por comillas simples('), tu respuesta será procesada posteriormente y es imperativo que no hayan comillas dentro de los strings. 
+    Tu respuesta debe ser coherente con el formato JSON especificado.¡Tú puedes!
     """
     
 offendEs = load_dataset("fmplaza/offendes")
@@ -63,14 +148,26 @@ sexismreddit = load_dataset("natural-lang-processing/sexismreddit")
 
 pd_sexism_train = pd.DataFrame(sexismreddit['train'])
 pd_sexism_train_text = pd_sexism_train[['text', 'label_vector']].copy()
-X = pd_sexism_train_text['text']
+pd_sexism_train_text = pd_sexism_train_text.drop(pd_sexism_train_text[pd_sexism_train_text['label_vector']=='none'].index)
+pd_sexism_train_text.replace('"', "'", regex=True, inplace=True)
+pd_sexism_train_text.replace('“', "'", regex=True, inplace=True)
+pd_sexism_train_text.replace('”', "'", regex=True, inplace=True)
+X= pd_sexism_train_text['text']
 y = pd_sexism_train_text['label_vector']
 
-X_train, X_test,y_train,y_test = train_test_split(X, y, stratify=y, test_size=0.5)
 print("valores de entrenamiento\n")
-print(y_train.value_counts(), "\n")
-print("valores de test\n")
-print(y_test.value_counts(), "\n")
+print(y.value_counts(), "\n")
+
 pandas_offendES_train = pd.DataFrame(offendEs['train'])
+pandas_offendES_validation = pd.DataFrame(offendEs['validation'])
+pandas_offendES_test = pd.DataFrame(offendEs['test'])
+
 new_offendEs_train = pandas_offendES_train[['comment', 'label']].copy()
-print(new_offendEs_train.sample(5))
+new_offendEs_train = new_offendEs_train.rename(columns={'comment':'text'})
+
+new_offendEs_validation = pandas_offendES_validation[['comment', 'label']].copy()
+new_offendEs_validation = new_offendEs_validation.rename(columns={'comment':'text'})
+
+new_offendEs_test = pandas_offendES_test[['comment', 'label']].copy()
+new_offendEs_test = new_offendEs_test.rename(columns={'comment':'text'})
+
